fee866cb76537ca3063757302daf4c295b47c38a
Mahmoud Higazy <ma7moudhigazy@gmail.com>
2019-10-13 03:10:16 +0200

    Added billing and client side app! comment build on .gitignore file

---
+++ client/build/precache-manifest.ab0fcc170371a2ce7d7795206f01ca33.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c3cbeeb1b022a99761811c4d9c9a1213",
+    "url": "/index.html"
+  },
+  {
+    "revision": "44ac8ff80a8f0be68e78",
+    "url": "/static/css/2.e7604903.chunk.css"
+  },
+  {
+    "revision": "44ac8ff80a8f0be68e78",
+    "url": "/static/js/2.d0ccf657.chunk.js"
+  },
+  {
+    "revision": "ac598d19d82a881ee344",
+    "url": "/static/js/main.1ec4c1bf.chunk.js"
+  },
+  {
+    "revision": "2055ac218fe3f7ad47ca",
+    "url": "/static/js/runtime-main.a7ca617d.js"
+  }
+]);
